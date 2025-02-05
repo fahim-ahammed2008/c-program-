19a6ab3a03a8f85c02271de07ade43a16ea97da2
Md Fahim Ahammed <mdfahimahammed347@gmail.com>
2025-02-05 23:42:31 +0600

    Add files via upload

---
+++ class38.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+
+  //  int num1,num2,sum;
+  float num1,num2,sum;
+
+    printf("Enter 1st Number:");
+
+    //scanf("%d",&num1);
+
+    scanf("%f",&num1);
+
+    printf("Enter 2nd Number:");
+
+    //scanf("%d",&num2);
+     
+    scanf("%f",&num2); 
+
+    sum = num1 + num2 ;
+    
+    //printf ("The Sum Is : %d\n",sum);
+     
+    printf ("The Sum Is : %.3f\n",sum); 
+
+  
+
+
+
+
+
+}
